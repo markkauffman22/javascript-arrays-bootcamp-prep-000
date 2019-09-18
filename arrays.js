dfc9d760ef8addbe99b6b1306a45d2cde7bc50db
Mark Kauffman <markkauffman22@gmail.com>
2019-09-18 04:14:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -13,3 +13,31 @@ function destructivelyAddElementToBeginningOfArray(array) {
   snot
 }
 
+/*//////////////////////////////
+  describe('addElementToBeginningOfArray(array, element)', () => {
+    it('adds an element to the beginning of an array', () => {
+      expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
+    })
+
+    it('does not alter the original array', () => {
+      const array = [1]
+
+      addElementToBeginningOfArray(array, 'foo')
+
+      expect(array).to.eql([1])
+    })
+  })
+
+  describe('destructivelyAddElementToBeginningOfArray(array, element)', () => {
+    it('adds an element to the beginning of an array', () => {
+      expect(destructivelyAddElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
+    })
+
+    it('alters the original array', () => {
+      const array = [1]
+
+      destructivelyAddElementToBeginningOfArray(array, 'foo')
+
+      expect(array).to.eql(['foo', 1])
+    })
+*////////////////////////
