cc20b712c20514db5290e2df89b8e35f29f36dda
Mark Kauffman <markkauffman22@gmail.com>
2019-09-18 05:21:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -40,18 +40,8 @@ function removeElementFromEndOfArray(array) {
   return array
 }
 
+function 
 /*//////////////////////////////
 
-  describe('removeElementFromEndOfArray(array)', () => {
-    it('removes the last element from the array', () => {
-      expect(removeElementFromEndOfArray([1, 2, 3])).to.eql([1, 2])
-    })
-    
-    it('does not alter the original array', () => {
-      const array = [1, 2, 3];
-      removeElementFromEndOfArray(array);
-      expect(array).to.eql([1, 2, 3]);
-    })
-  })
-})
+
 *////////////////////////
