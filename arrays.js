fc6df51ee3d3084b212ffebc7fa114de4f205a5b
Mark Kauffman <markkauffman22@gmail.com>
2019-09-18 05:16:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -37,32 +37,6 @@ function removeElementFromBeginningOfArray(array) {
 
 /*//////////////////////////////
 
-  describe('', () => {
-    it('removes the first element from the array', () => {
-      expect(removeElementFromBeginningOfArray([1, 2, 3])).to.eql([2, 3])
-    })
-
-    it('does not alter the original array', () => {
-      const array = [1, 2, 3];
-
-      removeElementFromBeginningOfArray(array);
-
-      expect(array).to.eql([1, 2, 3]);
-    })
-  })
-
-  describe('destructivelyRemoveElementFromEndOfArray(array)', () => {
-    it('returns the array with the last element removed', () => {
-      expect(destructivelyRemoveElementFromEndOfArray([1, 2, 3])).to.eql([1, 2])
-    })
-    
-    it('alters the original array', ()=>{
-      const array = [1, 2, 3];
-      destructivelyRemoveElementFromEndOfArray(array);
-      expect(array).to.eql([1, 2]);
-    })
-  })
-  
   describe('removeElementFromEndOfArray(array)', () => {
     it('removes the last element from the array', () => {
       expect(removeElementFromEndOfArray([1, 2, 3])).to.eql([1, 2])
