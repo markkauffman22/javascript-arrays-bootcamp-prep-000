7092e17664cbd3a7aaa2bc03d1d05452edf4a643
Mark Kauffman <markkauffman22@gmail.com>
2019-09-18 04:41:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element);
+  array = array.array;
   
 }
 
