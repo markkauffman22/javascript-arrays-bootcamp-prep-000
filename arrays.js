305b05ee479b7f2a419dbfd5dadcd8c91b34c356
Mark Kauffman <markkauffman22@gmail.com>
2019-09-18 05:31:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -34,8 +34,10 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
 }
-destructivelyRemoveElementFromEndOfArray(array) {
-  
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
 }
 
 function removeElementFromEndOfArray(array) {
