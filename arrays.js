7c030f79747d68f1da24cba164a60773fbf35c3e
Mark Kauffman <markkauffman22@gmail.com>
2019-09-18 05:05:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,9 +28,9 @@ function accessElementInArray(array, index) {
   return array[index];
 }
   
-function destructivelyRemoveElementFromBeginningOfArray(array){
+function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array.shift()
-  
+}  
 
 /*//////////////////////////////
 
